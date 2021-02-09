24b27374953d64272fd28cad9f74a42f223038a2
YunxiangZhao <78027182+YunxiangZhao@users.noreply.github.com>
2021-02-09 16:26:41 +0800

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from rango.models import Category, Page
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page)
-# Register your models here.
